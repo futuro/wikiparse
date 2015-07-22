1d0d1543a2536f48f7a8ef38866bd76453d85c1f
futuro <eniessenderry@gmail.com>
2015-07-22 13:45:03 -0500

    added some db dependencies

--- project.clj
+++ project.clj
@@ -7,7 +7,10 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
+                 [org.clojure/core.incubator "0.1.3"]
                  [org.clojure/data.xml "0.0.8"]
+                 [org.clojure/java.jdbc "0.3.7"]
                  [org.clojure/tools.cli "0.2.4"]
                  [clojurewerkz/elastisch "2.2.0-beta3"]
-                 [org.apache.commons/commons-compress "1.9"]])
+                 [org.apache.commons/commons-compress "1.9"]
+                 [org.postgresql/postgresql "9.4-1201-jdbc41"]])
